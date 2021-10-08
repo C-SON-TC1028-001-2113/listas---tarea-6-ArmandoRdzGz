52e2be820e93e0c342331ffff7be2529c1c9c00c
ArmandoRdzGz <A01254249@itesm.mx>
2021-10-08 01:56:32 +0000

    t6_fibonacci_armando

--- assignments/09SerieFibonacciListas/src/exercise.py
+++ assignments/09SerieFibonacciListas/src/exercise.py
@@ -1,5 +1,22 @@
 def main():
+    fibonacci = [0,1]
+    lista = []
+    a = [0]
+    b = -1
     
+    while b<0:
+        b = int(input(''))
+        if b == 0:
+            print (lista)
+        elif b>0:
+            if b ==1:
+                print (a)
+            elif b==2:
+                print (fibonacci)
+            elif b>2:
+                for x in range (b-2):
+                    fibonacci.append(fibonacci[x]+fibonacci[x+1])
+                print (fibonacci)    
     pass
 
 if __name__=='__main__':
